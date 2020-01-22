2865be84510747f55ec2265df297aa581c70f668
okazakishoma <okazakishoma@team-lab.com>
2020-01-22 21:56:06 +0900

    ログイン機能の途中まで

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template lang="pug">
 v-app
-  v-card(
-    color="grey lighten-4"
-    flat
-    height="200px"
-    title
+  v-app-bar(
+    app
+    color="primary"
+    dark
   )
-    v-toolbar(dense)
-      v-app-bar-nav-icon
-      v-toolbar-title マイアドレス帳
-      v-spacer
+    v-app-bar-nav-icon(
+      @click="toggleSideMenu"
+    )
+    v-toolbar-title マイアドレス帳
+    v-spacer
+  SideNav
+  v-content
+    v-container(
+      fluid
+      fill-height
+      align-start
+    )
+      router-view
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { mapActions } from 'vuex';
+import SideNav from './components/SideNav';
 
 export default Vue.extend({
   name: 'App',
-
   components: {
+    SideNav,
   },
-
   data: () => ({
     //
   }),
+  methods: {
+    ...mapActions(['toggleSideMenu']),
+  }
 });
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    drawer: false,
+    addresses: [] as Array<any>,
   },
   mutations: {
+    toggleSideMenu(state){
+      state.drawer = !state.drawer
+    },
+    addAddress(state, address){
+      state.addresses.push(address)
+    }
   },
   actions: {
+    login(){
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(google_auth_provider);
+    },
+    toggleSideMenu({ commit }){
+      commit('toggleSideMenu')
+    },
+    addAddress({commit}, address){
+      commit('addAddress', address)
+    }
   },
   modules: {
   }
